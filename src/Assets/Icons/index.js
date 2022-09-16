dd1231f30f439d2d534faa74db7a9d65d3caa29b
Abolfazl-ghodrati-k <104020673+Abolfazl-ghodrati-k@users.noreply.github.com>
2022-09-16 15:21:50 +0430

    start of project

---
+++ src/Assets/Icons/index.js
@@ -0,0 +1,41 @@
+// Minimize icon windows
+import {FaRegWindowMinimize} from 'react-icons/faz'
+//Maximize icon windows
+import {VscChromeMaximize} from 'react-icons/vsc'
+//Cloas icon windows
+import {AiOutlineClose} from 'react-icons/ai'
+// App icons
+// Cmd
+import {VscTerminalCmd} from 'react-icons/vsc'
+// Todo
+import {FcTodoList} from 'react-icons/fc'
+// Chevron forward
+import {IoChevronForwardOutline} from 'react-icons/io'
+// Dot
+import {BsDot} from 'react-icons/bs'
+//Shut down
+import {GrPowerShutdown} from 'react-icons/gr'
+// Setting icon
+import {FiSettings} from 'react-icons/fi'
+//Resume icon 
+import Resume from './resume.png'
+// Contact us
+import {IoIosContact} from 'react-icons/io'
+//Folders
+import {BsFolder} from 'react-icons/bs'
+// File browser 
+import {MdOutlineOpenInBrowser} from 'react-icons/md'
+//Gmail
+import {FaGoogle} from 'react-icons/fa'
+//Telegram
+import {FaTelegram} from 'react-icons/fa'
+//Instagram 
+import {FaInstagram} from 'react-icons/fa'
+//Linkedin
+import {BsLinkedin} from 'react-icons/bs'
+//Twitter
+import {FaTwitter} from 'react-icons/fa'
+//Phone 
+import {AiTwotonePhone} from 'react-icons/ai'
+//whatsapp
+import {FaWhatsapp} from 'react-icons/fa'
