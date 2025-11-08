1899c26ef07428076c57992924cbfa911d8af33d
amu.ghodrat <ghodrati.abolfazl@tapsi.shop>
2025-11-08 17:49:16 +0330

    fix: build issue

--- src/pages/Portfolio/components/canvas/Ball.tsx
+++ src/pages/Portfolio/components/canvas/Ball.tsx
@@ -20,7 +20,7 @@ const Ball = (props) => {
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color='#fff8eb'
+          color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
@@ -30,8 +30,6 @@ const Ball = (props) => {
           rotation={[2 * Math.PI, 0, 6.25]}
           scale={1}
           map={decal}
-          // @ts-expect-error TS(2322): Type '{ position: [number, number, number]; rotati... Remove this comment to see the full error message
-          flatShading
         />
       </mesh>
     </Float>
@@ -41,7 +39,7 @@ const Ball = (props) => {
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
     >
